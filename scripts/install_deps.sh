a98b1c2cf67678fb7f39dc8f195ea4f22de5d260
ritesh355 <riteshsingh86991@gmail.com>
2025-10-27 18:49:19 +0530

    updating buildspec and install_deps.sh

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/my-app
 rm -rf node_modules package-lock.json
-npm install --cache /home/ubuntu/.npm 
+npm install --cache /home/ubuntu/.npm --no-audit
+chmod -R u+w node_modules package-lock.json 2>/dev/null || true 
