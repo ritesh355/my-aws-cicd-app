42cc5593680556ebc57681fffa83967ae3575be1
ritesh355 <riteshsingh86991@gmail.com>
2025-10-27 19:57:09 +0530

    small change for checking pipeline

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from AWS CI/CD!'));
+app.get('/', (req, res) => res.send('Hello from AWS CI/CD!I name is Ritesh'));
 app.listen(3000, () => console.log('App running on port 3000'));
